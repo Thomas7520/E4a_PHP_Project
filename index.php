eec3130890a478bad464e16e133537cfd3a7b822
GERARD Thomas <thomas.gerard@reseau.eseo.fr>
2025-11-04 14:53:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Helpers\Psr4AutoloaderClass;
+
+require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+
+$loader = new Psr4AutoloaderClass();
+
+$loader->register();
+
+$loader->addNamespace('Helpers', __DIR__ . '/Helpers');
+$loader->addNamespace('League\Plates', __DIR__ . '/Vendor/Plates/src');
+$loader->addNamespace('Controllers', __DIR__ . '/Controllers');
+
+
+use Controllers\MainController;
+$controller = new MainController();
+$controller->index();
